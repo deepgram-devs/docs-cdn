c446cfd53cefb3cf549d4ca4129f7a2a9f48a6f3
Luke Oliff <luke@lukeoliff.com>
2023-05-08 18:37:57 -0700

    use window.online instead of anonymous function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-(function () {
+window.onload = function () {
   var external = document.querySelectorAll('[target="_self"][to*="https://"]');
 
   external.forEach(function (el) {
     el.href = el.getAttribute("to");
     el.target = "_blank";
   });
-})();
+};
