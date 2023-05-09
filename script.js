5a7d6252b990ac23350a4a2e2e8ae41e239a1ac0
Luke Oliff <luke@lukeoliff.com>
2023-05-09 10:46:52 -0700

    clear console

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 window.onload = function () {
+  // make top-nav external links target="_blank"
   var external = document.querySelectorAll('[target="_self"][to*="https://"]');
 
   external.forEach(function (el) {
     el.href = el.getAttribute("to");
     el.target = "_blank";
   });
+
+  // clear console of readme marketing
+  console.clear();
 };
