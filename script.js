93658e19d9215c78fe739d72b2731a1a00fdbe59
Luke Oliff <luke@lukeoliff.com>
2023-05-19 15:11:55 -0700

    Better JS for after load changes (#14)
    * registering a mutationobserver to make DOM changes after mutations occur, rather than just page loads
    
    * moving stuff about
    
    * register if node exists. change from reference body ID to entire body ID

--- script.js
+++ script.js
@@ -1,19 +1,80 @@
-window.onload = function () {
-  // make top-nav external links target="_blank"
+/**
+ * make top-nav external links target="_blank"
+ */
+function convertHttpLinkstoExternal() {
   var external = document.querySelectorAll('[target="_self"][to*="https://"]');
 
   external.forEach(function (el) {
     el.href = el.getAttribute("to");
     el.target = "_blank";
   });
+}
 
-  // exclude the playground from fullstory
+/**
+ * Exclude the playground from fullstory.
+ */
+function excludePlayergroundFromAnalytics() {
   var playground = document.getElementById("ReferencePlayground");
   if (playground) {
     playground.classList.add("fs-exclude");
     playground.setAttribute("data-heap-redact-text", "");
   }
+}
 
-  // clear console of readme marketing
+/**
+ * Registers a MutationObserver to the first object with the specified value of the ID attribute.
+ * @param {string} elementId String that specifies the ID value.
+ */
+function registerObserverById(elementId) {
+  /**
+   * Get element to observe
+   */
+  const targetNode = document.getElementById(elementId);
+
+  if (targetNode) {
+    /**
+     * Options for the observer (which mutations to observe).
+     */
+    const config = { childList: true, subtree: true };
+
+    /**
+     * Callback function to execute when mutations are observed.
+     */
+    const callback = () => {
+      observableChanges();
+    };
+
+    /**
+     * Create an observer instance linked to the callback function.
+     */
+    const observer = new MutationObserver(callback);
+
+    /**
+     * Start observing the target node for configured mutations.
+     */
+    observer.observe(targetNode, config);
+  }
+}
+
+/**
+ * Changes once the page has initialized.
+ */
+function launchChanges() {
+  convertHttpLinkstoExternal();
+  excludePlayergroundFromAnalytics();
   console.clear();
+}
+
+/**
+ * Changes that run when #Explorer mutations are observed.
+ */
+function observableChanges() {
+  convertHttpLinkstoExternal();
+  excludePlayergroundFromAnalytics();
+}
+
+window.onload = function () {
+  // Initial page changes.
+  launchChanges();
+  registerObserverById("ssr-main"); // body by ID
 };
